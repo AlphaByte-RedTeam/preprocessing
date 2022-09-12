56e3eefc97852e391725a986b95b733d0075ebb4
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-09-12 09:29:41 +0700

    chore: change image display dimension to 250x250

--- backup/preprocessing.pas
+++ backup/preprocessing.pas
@@ -27,6 +27,7 @@ type
     openDialog: TOpenDialog;
     saveDialog: TSaveDialog;
     procedure btnBinaryClick(Sender: TObject);
+    procedure btnDilasiClick(Sender: TObject);
     procedure btnGrayClick(Sender: TObject);
     procedure btnSaveClick(Sender: TObject);
     procedure btnSharpClick(Sender: TObject);
@@ -226,6 +227,80 @@ begin
       begin
         bmpBiner[x,y] := 1;
         imgMod.Canvas.Pixels[x, y] := RGB(255, 255, 255);
+      end;
+    end;
+  end;
+end;
+
+procedure TForm1.btnDilasiClick(Sender: TObject);
+var
+  i, j, k, l  : integer;
+  temp : boolean;
+  objek, latar : integer;
+begin
+  //objek berwarna hitam
+  objek := 1;
+  latar := 0;
+
+  for j := -1 to 1 do
+  begin
+    for i := -1 to 1 do
+    begin
+      SE[i,j] := 1;
+    end;
+  end;
+
+  for j:=0 to imgSrc.Height-1 do
+  begin
+    for i:=0 to imgSrc.Width-1 do
+    begin
+          temp := True;
+          for l:=-1 to 1 do
+          begin
+            for k:=-1 to 1 do
+            begin
+              if (SE[k,l]>=0) then
+              begin
+                Temp := Temp AND (bmpBiner[i+k,j+l]=SE[k,l]);
+              end;
+            end;
+          end;
+          if temp then
+            hasilBiner[i,j] := objek
+          else
+            hasilBiner[i,j] := latar;
+          imgMod.Canvas.Pixels[i,j]:= RGB(hasilBiner[i,j]*255, hasilBiner[i,j]*255, hasilBiner[i,j]*255);
+          end;
+    end;
+
+  for j:=0 to imgSrc.Height-1 do
+  begin
+    for i:=0 to imgSrc.Width-1 do
+    begin
+      bmpBiner[i,j] := hasilBiner[i,j];
+      if j=0 then
+      begin
+        bmpR[i,-1] := bmpR[i,j];
+        bmpG[i,-1] := bmpG[i,j];
+        bmpB[i,-1] := bmpB[i,j];
+      end;
+      if j=imgSrc.Height-1 then
+      begin
+        bmpR[i,j+1] := bmpR[i,j];
+        bmpG[i,j+1] := bmpG[i,j];
+        bmpB[i,j+1] := bmpB[i,j];
+      end;
+      if i=0 then
+      begin
+        bmpR[-1,j] := bmpR[i,j];
+        bmpG[-1,j] := bmpG[i,j];
+        bmpB[-1,j] := bmpB[i,j];
+      end;
+      if i=imgSrc.Width-1 then
+      begin
+        bmpR[i+1,j] := bmpR[i,j];
+        bmpG[i+1,j] := bmpG[i,j];
+        bmpB[i+1,j] := bmpB[i,j];
       end;
     end;
   end;
